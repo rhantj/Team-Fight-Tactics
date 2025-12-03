7299fc6262cf69607f9f874d1fabe1627867567a
권정민 <kopoma1235@gmail.com>
2025-12-03 16:13:55 +0900

    Commit : ObjectPooling System Develop

--- Assets/KWON/Scripts/ObjectPooling/PoolManager.cs
+++ Assets/KWON/Scripts/ObjectPooling/PoolManager.cs
@@ -15,7 +15,7 @@ public class PoolManager : MonoBehaviour
     }
 
     [Header("Pool Settig")]
-    public List<PoolConfig> pollConfigs = new List<PoolConfig>();
+    public List<PoolConfig> poolConfigs = new List<PoolConfig>();
     private Dictionary<string, ObjectPool<Component>> poolDict = new Dictionary<string, ObjectPool<Component>>();
 
     private void Awake()
@@ -23,6 +23,70 @@ public class PoolManager : MonoBehaviour
         if(Instance != null && Instance != this)
         {
             Destroy(gameObject);
+            return;
         }
+
+        Instance = this;
+
+        DontDestroyOnLoad(gameObject);
+
+        InitializePools();
+    }
+
+    //풀 준비(초기화)
+    private void InitializePools()
+    {
+        foreach(var config in poolConfigs)
+        {
+            if(string.IsNullOrEmpty(config.id)|| config.prefab == null)
+            {
+                Debug.LogWarning($"Invalid PoolConfig : {config.id}");
+                continue;
+            }
+
+            Component component = config.prefab.GetComponent<Component>();
+
+            var pool = new ObjectPool<Component>(
+                component,
+                config.preloadCount,
+                transform);
+
+            poolDict.Add(config.id, pool);
+        }
+    }
+
+    //풀 꺼내기
+    public GameObject Spawn(string id)
+    {
+        if (!poolDict.TryGetValue(id, out var pool))
+        {
+            Debug.LogError($"Pool ID '{id}' 없음");
+            return null;
+        }
+
+        Component compnent = pool.Get();
+        GameObject obj = compnent.gameObject;
+
+        var pooledObj = obj.GetComponent<PooledObject>();
+
+        if(pooledObj != null)
+        {
+            pooledObj.poolId = id;
+        }
+
+        return obj;
+    }
+
+    //풀 집어넣기
+    public void Despawn(string id, GameObject obj)
+    {
+        if(!poolDict.TryGetValue(id, out var pool))
+        {
+            Debug.LogError($"Pool ID '{id}' 없음");
+            Destroy(obj);
+            return;
+        }
+
+        pool.Release(obj.GetComponent<Component>());
     }
 }
---
+++ Assets/KWON/Scripts/ObjectPooling/PooledObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PooledObject : MonoBehaviour
+{
+    [HideInInspector]
+    public string poolId;
+
+    public void ReturnToPool()
+    {
+        if(!string.IsNullOrEmpty(poolId))
+        {
+            PoolManager.Instance.Despawn(poolId, gameObject);
+        }
+        else
+        {
+            Debug.LogWarning($"PooledObject '{name}' 풀 아이티 없음. ");
+            Destroy(gameObject);
+        }
+    }
+}
