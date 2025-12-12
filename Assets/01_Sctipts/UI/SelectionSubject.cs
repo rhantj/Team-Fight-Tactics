287253b1677bd4f63041f2cdf0839a75eca77ade
Roguree <touko1996@gmail.com>
2025-12-12 16:31:18 +0900

    Cleanup: remove unused SelectionSubject

--- Assets/01_Sctipts/UI/SelectionSubject.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using UnityEngine;
-
-public static class SelectionSubject
-{
-    // ChessStatData를 구독자(ChessInfoUI 등)에게 전달하는 이벤트
-    public static Action<ChessStatData> OnUnitSelected;
-}
