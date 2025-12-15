494b9c4bbed5d3bd3600125e5a5e637afbdcc100
rhantj <dddddfgh@gmail.com>
2025-12-15 10:51:37 +0900

    Commented on code Update Testing Field

--- Assets/01_Sctipts/Grid/FieldGrid.cs
+++ Assets/01_Sctipts/Grid/FieldGrid.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FieldGrid : GridDivideBase
 {
+    // 필드 위의 전체 기물 리스트
     public List<ChessStateBase> GetAllFieldUnits()
     {
         List<ChessStateBase> result = new();
--- Assets/01_Sctipts/Scripts/ObjectPooling/SFXManager.cs
+++ Assets/01_Sctipts/Scripts/ObjectPooling/SFXManager.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 /*
 Don't use Singleton pattern
 
-spatialBlend == 0 : bgm
-spatialBlend == 1 : other 
+spatialBlend == 0 : bgm(2d sound)
+spatialBlend == 1 : other(3d sound)
  */
 
+// 실질적으로 외부에서 사용할 사운드 시스템
 public static class SoundSystem
 {
+    // SoundSystem.SoundPlayer.Play(...)로 싱글톤처럼 사용 가능
     public static ISoundable SoundPlayer { get; private set; }
     public static void DI(ISoundable soundPlayer)
     {
@@ -20,9 +22,9 @@ public static class SoundSystem
 
 public class SFXManager : MonoBehaviour, ISoundable
 {
-    [SerializeField] AudioClip[] preloadSFX;
-    private Dictionary<string, AudioClip> clipDic = new();
-    public List<GameObject> usingSound = new();
+    [SerializeField] AudioClip[] preloadSFX;                // 효과음 캐싱
+    private Dictionary<string, AudioClip> clipDic = new();  // 효과음 분류
+    public List<GameObject> usingSound = new();             // 사용중인 사운드
 
     private void Awake()
     {
@@ -34,6 +36,7 @@ public class SFXManager : MonoBehaviour, ISoundable
         SoundSystem.DI(this);
     }
 
+    // 사운드 실행(클립이름, 위치, 볼륨, 2d/3d)
     public void PlaySound(string name, Vector3 pos, float volume = 1f, float spatialBlend = 0f)
     {
         if (!clipDic.ContainsKey(name)) return;
@@ -47,6 +50,7 @@ public class SFXManager : MonoBehaviour, ISoundable
         sfx.Play(clip, volume, spatialBlend);
     }
 
+    // 씬이 변경되거나 bgm 종료 시 사용
     public void StopAllSound()
     {
         foreach(var audio in usingSound)
