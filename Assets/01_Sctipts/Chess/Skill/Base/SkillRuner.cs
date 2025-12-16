ae3e57626af2560edcf9922a59652d742dfe113f
HKgoat <baekhoymamber@gmail.com>
2025-12-16 16:56:46 +0900

    Add Skill Animation, Transition

---
+++ Assets/01_Sctipts/Chess/Skill/Base/SkillRuner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class SkillRunner : MonoBehaviour
+{
+    private Chess chess;               
+    private Animator animator;
+    private StateMachine sm;
+
+    private bool isCasting;
+
+    private void Awake()
+    {
+        chess = GetComponent<Chess>();
+        animator = GetComponent<Animator>();
+        sm = GetComponent<StateMachine>();   
+    }
+
+    public void RequestCast()
+    {
+        if (isCasting) return;
+
+        SkillBase skill = GetComponent<SkillBase>();
+        if (skill == null) return;
+
+        StartCoroutine(CastRoutine(skill));
+    }
+
+    private IEnumerator CastRoutine(SkillBase skill)
+    {
+        isCasting = true;
+
+        chess.overrideState = true; 
+
+        sm?.SetSkill();
+        animator?.SetTrigger("Skill");
+        yield return skill.Execute(chess);
+
+        chess.overrideState = false;
+        sm?.SetBattle();
+
+        isCasting = false;
+    }
+}
