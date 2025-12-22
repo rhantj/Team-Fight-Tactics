43474b5aab76b76a6e3caf9a430689aeda0ec630
Roguree <touko1996@gmail.com>
2025-12-22 11:52:37 +0900

    SlotImage Edit

--- Assets/01_Sctipts/UI/ChessInfoItemSlot.cs
+++ Assets/01_Sctipts/UI/ChessInfoItemSlot.cs
@@ -2,46 +2,43 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ChessInfoItemSlot :
-    MonoBehaviour,
-    IPointerEnterHandler,
-    IPointerExitHandler,
-    IPointerDownHandler
+public class ChessInfoItemSlot : MonoBehaviour,
+    IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
     [SerializeField] private Image iconImage;
 
+    [Header("Default Slot")]
+    [SerializeField] private Sprite defaultSlotSprite;
+
     private ItemData currentItem;
 
-    public bool HasItem => currentItem != null;
-
-    // ChessInfoUI에서 호출
     public void SetItem(ItemData item)
     {
         currentItem = item;
 
         if (item == null)
         {
-            iconImage.sprite = null;
-            iconImage.enabled = false;
-            return;
+            // 빈 슬롯
+            iconImage.sprite = defaultSlotSprite;
+            iconImage.color = Color.white;
         }
-
-        iconImage.sprite = item.icon;
-        iconImage.enabled = true;
+        else
+        {
+            // 아이템 있음
+            iconImage.sprite = item.icon;
+            iconImage.color = Color.white;
+        }
     }
 
     public void Clear()
     {
-        currentItem = null;
-        iconImage.sprite = null;
-        iconImage.enabled = false;
+        SetItem(null);
     }
 
-    // ================== 마우스 오버 ==================
+    // 마우스 오버 / 우클릭은 item != null 일 때만 반응
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (currentItem == null) return;
-
         ItemInfoUIManager.Instance.Show(currentItem);
     }
 
@@ -51,7 +48,6 @@ public class ChessInfoItemSlot :
         ItemRecipeUIManager.Instance.Hide();
     }
 
-    // ================== 우클릭 ==================
     public void OnPointerDown(PointerEventData eventData)
     {
         if (currentItem == null) return;
