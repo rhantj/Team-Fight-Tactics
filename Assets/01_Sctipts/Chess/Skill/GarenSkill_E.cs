904fec88ba3bfc92c25f6cc59ee2e892513f2936
HKgoat <baekhoymamber@gmail.com>
2025-12-22 18:08:51 +0900

    fixed SkillManager, Add SkillBase,Fixed Garen Skill_E

--- Assets/01_Sctipts/Chess/Skill/GarenSkill_E.cs
+++ Assets/01_Sctipts/Chess/Skill/GarenSkill_E.cs
@@ -24,10 +24,25 @@ public class GarenSkill_E : SkillBase
     [Tooltip("Hit Effect")]
     [SerializeField] private GameObject hitVfxPrefab;
 
+    [Header("Animator")]
+    [Tooltip("Animator Bool 파라미터 이름")]
+    [SerializeField] private string isSpinningBool = "IsSpinning";
+
+
+    private void Awake()
+    {
+        repeatCount = 3;       
+        repeatInterval = 0f;
+        //endByAnimEvent = false;
+        //blockUpdateWhileCasting = false;
+    }
     public override IEnumerator Execute(ChessStateBase caster)
     {
         Chess garen = caster as Chess;
         if (garen == null) yield break;
+        Animator anim = garen.GetComponent<Animator>();
+        if (anim != null && !string.IsNullOrEmpty(isSpinningBool))
+            anim.SetBool(isSpinningBool, true);
         //누굴 공격할지.
         List<Chess> enemyList = (garen.team == Team.Player)
             ? UnitCountManager.Instance.enemyUnits
@@ -70,7 +85,8 @@ public class GarenSkill_E : SkillBase
 
             yield return null;
         }
-
+        if (anim != null && !string.IsNullOrEmpty(isSpinningBool))
+            anim.SetBool(isSpinningBool, false);
         if (spinVfx != null)
             Object.Destroy(spinVfx);
     }
