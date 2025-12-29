90154c7aa5355132926e2061983ede4faea46923
rhantj <dddddfgh@gmail.com>
2025-12-29 18:22:33 +0900

    Update Enemy Stat

--- Assets/01_Sctipts/Chess/Enemy/Enemy.cs
+++ Assets/01_Sctipts/Chess/Enemy/Enemy.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class Enemy : Chess
 {
-    Dictionary<string, List<float>> statPerRound;
+    public Dictionary<string, List<float>> statPerRound;
 
     protected override void Awake()
     {
@@ -49,11 +49,18 @@ public class Enemy : Chess
     public void SetStats(int round)
     {
         InitFromSO();
+        int idx = round - 1;
 
-        baseData.maxHP = (int)statPerRound["maxHp"][round - 1];
-        baseData.armor = (int)statPerRound["armor"][round - 1];
-        baseData.attackDamage = (int)statPerRound["attackDamage"][round - 1];
-        baseData.attackSpeed = statPerRound["attackSpeed"][round - 1];
-        baseData.mana = (int)statPerRound["mana"][round - 1];
+        baseData.maxHP = (int)statPerRound["maxHp"][idx];
+        baseData.armor = (int)statPerRound["armor"][idx];
+        baseData.attackDamage = (int)statPerRound["attackDamage"][idx];
+        baseData.attackSpeed = statPerRound["attackSpeed"][idx];
+        baseData.mana = (int)statPerRound["mana"][idx];
+
+        //baseData.maxHP = (int)statPerRound["maxHp"].values[idx];
+        //baseData.armor = (int)statPerRound["armor"].values[idx];
+        //baseData.attackDamage = (int)statPerRound["attackDamage"].values[idx];
+        //baseData.attackSpeed = statPerRound["attackSpeed"].values[idx];
+        //baseData.mana = (int)statPerRound["mana"].values[idx];
     }
 }
