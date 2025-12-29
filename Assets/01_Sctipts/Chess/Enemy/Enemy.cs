f9fc31d75a0fadac2930cdfd80b27c632d821011
rhantj <dddddfgh@gmail.com>
2025-12-29 18:45:28 +0900

    Update Chess Layer

--- Assets/01_Sctipts/Chess/Enemy/Enemy.cs
+++ Assets/01_Sctipts/Chess/Enemy/Enemy.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class Enemy : Chess
 {
-    public Dictionary<string, List<float>> statPerRound;
+    public StringFloatListDict statPerRound;
 
     protected override void Awake()
     {
@@ -14,7 +14,7 @@ public class Enemy : Chess
 
         string[] objName = gameObject.name.Split("(Clone)");
 
-        statPerRound = CSVReader.BuildStatPerRound(objName[0]);
+        statPerRound = CSVReader.BuildStatPerRoundSD(objName[0]);
     }
     private void Start()
     {
@@ -48,19 +48,19 @@ public class Enemy : Chess
 
     public void SetStats(int round)
     {
-        InitFromSO();
         int idx = round - 1;
 
-        baseData.maxHP = (int)statPerRound["maxHp"][idx];
-        baseData.armor = (int)statPerRound["armor"][idx];
-        baseData.attackDamage = (int)statPerRound["attackDamage"][idx];
-        baseData.attackSpeed = statPerRound["attackSpeed"][idx];
-        baseData.mana = (int)statPerRound["mana"][idx];
+        //baseData.maxHP = (int)statPerRound["maxHp"][idx];
+        //baseData.armor = (int)statPerRound["armor"][idx];
+        //baseData.attackDamage = (int)statPerRound["attackDamage"][idx];
+        //baseData.attackSpeed = statPerRound["attackSpeed"][idx];
+        //baseData.mana = (int)statPerRound["mana"][idx];
 
-        //baseData.maxHP = (int)statPerRound["maxHp"].values[idx];
-        //baseData.armor = (int)statPerRound["armor"].values[idx];
-        //baseData.attackDamage = (int)statPerRound["attackDamage"].values[idx];
-        //baseData.attackSpeed = statPerRound["attackSpeed"].values[idx];
-        //baseData.mana = (int)statPerRound["mana"].values[idx];
+        baseData.maxHP = (int)statPerRound["maxHp"].values[idx];
+        baseData.armor = (int)statPerRound["armor"].values[idx];
+        baseData.attackDamage = (int)statPerRound["attackDamage"].values[idx];
+        baseData.attackSpeed = statPerRound["attackSpeed"].values[idx];
+        baseData.mana = (int)statPerRound["mana"].values[idx];
+        //InitFromSO();
     }
 }
